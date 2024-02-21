0ab0fcf62b782cd95d44a3ead53b7fb33d6880ec
Ujjawal Joshi <ujjawaljoshi07@gmail.com>
2024-02-21 15:27:38 +0530

    started building api. handled get request

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
+const fs = require('fs')
 const express = require('express')
+
+const app = express()
+
+// JSON.parse converts to an array of JS objects
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+)
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: "sucess",
+        results: tours.length,
+        data: {
+            tours
+        }
+    })
+})
+
+const port = 3000
+app.listen(port, () => {
+    console.log(`App running on port ${port}...`)
+})
