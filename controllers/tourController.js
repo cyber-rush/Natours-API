02587d1c3e1305654e1bc426312afbbcbb46aba5
Ujjawal Joshi <ujjawaljoshi07@gmail.com>
2024-02-24 16:24:33 +0530

    setting up the env variables

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,6 +5,30 @@ const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 )
 
+exports.checkID = (req, res, next, val) => {
+    console.log(`Tour ID is : ${val}`)
+
+    if (req.params.id * 1 >= tours.length) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    }
+
+    next()
+}
+
+exports.checkBody = (req, res, next) => {
+    if (!req.body.name || !req.body.price) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Missing name or price'
+        })
+    }
+
+    next()
+}
+
 exports.getAllTours = (req, res) => {
     res.status(200).json({
         status: "sucess",
@@ -18,13 +42,6 @@ exports.getAllTours = (req, res) => {
 exports.getTour = (req, res) => {
 
     const id = req.params.id * 1 // converts string to Number
-
-    if (id >= tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        })
-    }
     const tour = tours.find(el => el.id === id)
     res.status(200).json({
         status: "sucess",
@@ -55,15 +72,6 @@ exports.createTour = (req, res) => {
 
 exports.updateTour = (req, res) => {
 
-    const id = req.params.id * 1 // converts string to Number
-
-    if (id >= tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        })
-    }
-
     res.status(200).json({
         status: "sucess",
         data: {
@@ -73,15 +81,6 @@ exports.updateTour = (req, res) => {
 }
 
 exports.deleteTour = (req, res) => {
-
-    const id = req.params.id * 1 // converts string to Number
-
-    if (id >= tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        })
-    }
 
     res.status(204).json({
         status: "sucess",
