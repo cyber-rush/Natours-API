909f04681d1c7d52add8e2133d3d52b6ede54b75
Ujjawal Joshi <ujjawaljoshi07@gmail.com>
2024-03-06 17:33:21 +0530

    modelling the tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -46,9 +46,9 @@ exports.createTour = async (req, res) => {
 
     try {
 
-        await Tour.create(req.body)
+        const newTour = await Tour.create(req.body)
 
-        res.status(204).json({
+        res.status(201).json({
             status: "sucess",
             data: {
                 tour: newTour
@@ -59,7 +59,7 @@ exports.createTour = async (req, res) => {
     catch (err) {
         res.status(400).json({
             status: 'fail',
-            message: 'Invalid data sent!'
+            message: err
         })
     }
 
